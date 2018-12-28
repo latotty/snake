3b2b6b2c827ca70812cb9f7ba356a833de261358
LaTotty <latotty91@gmail.com>
2018-12-28 09:04:15 +0100

    now.sh compatibility

--- next.config.js
+++ next.config.js
@@ -1,11 +1,26 @@
-const withTypescript = require('@zeit/next-typescript');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const { PHASE_PRODUCTION_SERVER } =
+  process.env.NODE_ENV === 'development'
+    ? {} // We're never in "production server" phase when in development mode
+    : !process.env.NOW_REGION
+    ? require('next/constants') // Get values from `next` package when building locally
+    : require('next-server/constants'); // Get values from `next-server` package when building on now v2
 
-module.exports = withTypescript({
-  webpack(config, options) {
-    // Do not run type checking twice:
-    if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin());
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    // Config used to run in production.
+    return {};
+  }
 
-    return config;
-  },
-});
+  const withTypescript = require('@zeit/next-typescript');
+  const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+  return withTypescript({
+    webpack(config, options) {
+      // Do not run type checking twice:
+      if (options.isServer)
+        config.plugins.push(new ForkTsCheckerWebpackPlugin());
+
+      return config;
+    },
+  });
+};
