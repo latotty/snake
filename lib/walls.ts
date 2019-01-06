bf3371e25658d9da219639a7ab1a2e6fc45afcb1
LaTotty <latotty91@gmail.com>
2019-01-06 02:17:48 +0100

    Dynamic Walls and URL Query

--- lib/walls.ts
+++ lib/walls.ts
@@ -1,33 +1,51 @@
 import { Coord, tuple } from './coord';
 
-type Wall = [Coord, Coord];
+export type Wall = [Coord, Coord];
 
-const WALLS_FULL = [
-  tuple(tuple(0, 0), tuple(0, 38)), // LEFT
-  tuple(tuple(0, 39), tuple(38, 39)), // BOTTOM
-  tuple(tuple(39, 39), tuple(39, 1)), // RIGHT
-  tuple(tuple(39, 0), tuple(1, 0)), // TOP
+const WALLS_FULL = (width: number, height: number) => [
+  tuple(tuple(0, 0), tuple(0, height - 2)), // LEFT
+  tuple(tuple(0, height - 1), tuple(width - 2, height - 1)), // BOTTOM
+  tuple(tuple(width - 1, height - 1), tuple(width - 1, 1)), // RIGHT
+  tuple(tuple(width - 1, 0), tuple(1, 0)), // TOP
 ];
 
-const WALLS_NO: Wall[] = [];
+const WALLS_NO: Wall[] = (width: number, height: number) => [];
 
-const WALLS_CORNERS = [
-  tuple(tuple(0, 0), tuple(0, 11)), // LEFT_TOP
-  tuple(tuple(0, 28), tuple(0, 38)), // LEFT_BOTTOM
-  tuple(tuple(0, 39), tuple(11, 39)), // BOTTOM_LEFT
-  tuple(tuple(28, 39), tuple(38, 39)), // BOTTOM_RIGHT
-  tuple(tuple(39, 39), tuple(39, 28)), // RIGHT_TOP
-  tuple(tuple(39, 11), tuple(39, 1)), // RIGHT_BOTTOM
-  tuple(tuple(39, 0), tuple(28, 0)), // TOP_RIGHT
-  tuple(tuple(11, 0), tuple(1, 0)), // TOP_LEFT
+const WALLS_CORNERS = (width: number, height: number) => [
+  tuple(tuple(0, 0), tuple(0, Math.floor(height / 4))), // LEFT_TOP
+  tuple(tuple(0, height - Math.floor(height / 4) - 1), tuple(0, height - 1)), // LEFT_BOTTOM
+  tuple(tuple(0, height - 1), tuple(Math.floor(width / 4), height - 1)), // BOTTOM_LEFT
+  tuple(
+    tuple(width - Math.floor(width / 4) - 1, height - 1),
+    tuple(width - 2, height - 1),
+  ), // BOTTOM_RIGHT
+  tuple(
+    tuple(width - 1, height - 1),
+    tuple(width - 1, height - Math.floor(height / 4) - 1),
+  ), // RIGHT_TOP
+  tuple(tuple(width - 1, Math.floor(height / 4)), tuple(width - 1, 1)), // RIGHT_BOTTOM
+  tuple(tuple(width - 1, 0), tuple(width - Math.floor(width / 4) - 1, 0)), // TOP_RIGHT
+  tuple(tuple(Math.floor(width / 4), 0), tuple(1, 0)), // TOP_LEFT
 ];
 
-const WALLS_CROSS = [
-  tuple(tuple(19, 0), tuple(20, 39)), // TOP-BOTTOM
-  tuple(tuple(0, 19), tuple(39, 20)), // LEFT-RIGHT
+const WALLS_CROSS = (width: number, height: number) => [
+  tuple(
+    tuple(Math.floor(width / 2) - 1, 0),
+    tuple(Math.ceil(width / 2), height - 1),
+  ), // TOP-BOTTOM
+  tuple(
+    tuple(0, Math.floor(height / 2) - 1),
+    tuple(width - 1, Math.ceil(height / 2)),
+  ), // LEFT-RIGHT
 ];
 
-export const WALLS = [
+export interface WallsDef {
+  name: string;
+  key: string;
+  value: (width: number, height: number) => Wall[];
+}
+
+export const WALLS: WallsDef[] = [
   {
     name: 'Cross',
     key: 'cross',
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,31 @@
 import React, { useState, useCallback } from 'react';
+import Router from 'next/router';
 
 import * as snakeGame from '../game/snake';
 import { SnakeView } from '../components/snake-view';
-import { WALLS } from '../lib/walls';
+import { WALLS, WallsDef } from '../lib/walls';
 import { useManualSnake } from '../lib/manual-snake.hook';
 
 const BASE_TIMEOUT = 200;
 const BASE_SPEED = 1;
+const BOARD_SIZE = 11;
 
-export default () => {
+const getRandomSeed = () =>
+  Math.random()
+    .toString()
+    .slice(2);
+
+const getWallsByKey = (key: string) => WALLS.find(wall => wall.key === key);
+
+const IndexPage = ({ wallsKey, seed }: { wallsKey: string; seed: string }) => {
+  const wallsDef = getWallsByKey(wallsKey);
   const [snakeConfig, setSnakeConfig] = useState<snakeGame.Config>({
-    boardWidth: 40,
-    boardHeight: 40,
+    boardWidth: BOARD_SIZE,
+    boardHeight: BOARD_SIZE,
     initialSize: 3,
     foodValue: 0.1,
-    walls: WALLS[0].value,
-    seed: Math.random()
-      .toString()
-      .slice(2),
+    walls: wallsDef.value(BOARD_SIZE, BOARD_SIZE),
+    seed: seed,
   });
   const [speed, setSpeed] = useState(BASE_SPEED);
   const [vision, setVision] = useState<boolean>(false);
@@ -27,6 +35,14 @@ export default () => {
     speed,
     BASE_TIMEOUT,
   );
+
+  const changeSeed = (newSeed: string) => {
+    Router.push({
+      pathname: Router.pathname,
+      query: { ...Router.query, seed: newSeed },
+    });
+    setSnakeConfig(config => ({ ...config, seed: newSeed }));
+  };
 
   const resetGame = useCallback(() => setSnakeConfig(c => ({ ...c })), []);
   const increaseSpeed = useCallback(() => setSpeed(s => s + 1), []);
@@ -40,32 +56,27 @@ export default () => {
   }, []);
   const onSeedChange = useCallback(event => {
     event.persist();
-    setSnakeConfig(config => ({ ...config, seed: event.target.value }));
+    changeSeed(event.target.value);
   }, []);
   const randomizeSeed = useCallback(event => {
     event.persist();
-    setSnakeConfig(config => ({
-      ...config,
-      seed: Math.random()
-        .toString()
-        .slice(2),
-    }));
+    changeSeed(getRandomSeed());
   }, []);
 
   const onWallSelect = useCallback(event => {
     event.persist();
-    const def = WALLS.find(wall => wall.key === event.target.value);
+    const def = getWallsByKey(event.target.value);
     if (def) {
+      Router.push({
+        pathname: Router.pathname,
+        query: { ...Router.query, walls: event.target.value },
+      });
       setSnakeConfig((config: snakeGame.Config) => ({
         ...config,
-        walls: def.value,
+        walls: def.value(BOARD_SIZE, BOARD_SIZE),
       }));
     }
   }, []);
-
-  const wallKey = (
-    WALLS.find(wall => wall.value === snakeConfig.walls) || { key: 'unknown' }
-  ).key;
 
   return (
     <React.Fragment>
@@ -95,7 +106,7 @@ export default () => {
           </div>
           <div>
             Wall layout:
-            <select value={wallKey} onChange={onWallSelect}>
+            <select value={wallsDef.key} onChange={onWallSelect}>
               {WALLS.map(({ name, key }) => (
                 <option key={key} value={key}>
                   {name}
@@ -129,3 +140,25 @@ export default () => {
     </React.Fragment>
   );
 };
+
+IndexPage.getInitialProps = ({ query, res, pathname }) => {
+  const seed = query.seed || getRandomSeed();
+  const wallsKey = query.walls || WALLS[0].key;
+  const wallsDef = getWallsByKey(wallsKey);
+
+  if (!wallsDef) {
+    const url = `${pathname}?seed=${seed}&walls=${WALLS[0].key}`;
+    if (res) {
+      res.writeHead(302, {
+        Location: url,
+      });
+      res.end();
+    } else {
+      Router.push(url);
+    }
+  }
+
+  return { seed, wallsKey };
+};
+
+export default IndexPage;
